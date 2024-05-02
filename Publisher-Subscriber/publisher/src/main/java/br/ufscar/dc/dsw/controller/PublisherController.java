1ecd54bb88d655c0908bb82bdc2f10243082f644
Delano Medeiros Beder <delanobeder@yahoo.com.br>
2024-05-02 16:29:05 -0300

    Correção de bug

--- Publisher-Subscriber/publisher/src/main/java/br/ufscar/dc/dsw/controller/PublisherController.java
+++ Publisher-Subscriber/publisher/src/main/java/br/ufscar/dc/dsw/controller/PublisherController.java
@@ -3,7 +3,6 @@ package br.ufscar.dc.dsw.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.ufscar.dc.dsw.model.Message;
@@ -11,7 +10,7 @@ import br.ufscar.dc.dsw.service.RabbitMQSender;
 
 @RestController
 @RequestMapping(value = "/publisher/")
-public class RabbitMQWebController {
+public class PublisherController {
 
 	@Autowired
 	RabbitMQSender rabbitMQSender;
